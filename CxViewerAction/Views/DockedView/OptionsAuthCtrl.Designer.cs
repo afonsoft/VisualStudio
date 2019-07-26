0c99b601a0dd16108b5a4cd7d06e1a69732fd35b
Afonso Dutra Nogueira Filho <mzt.adnfilho@voegol.com.br>
2019-07-26 10:35:28 -0300

    Logo da GOL

--- CxViewerAction/Views/DockedView/OptionsAuthCtrl.Designer.cs
+++ CxViewerAction/Views/DockedView/OptionsAuthCtrl.Designer.cs
@@ -30,100 +30,115 @@ namespace CxViewerAction.Views.DockedView
         /// </summary>
         private void InitializeComponent()
         {
-			this.components = new System.ComponentModel.Container();
-			this.groupAuth = new System.Windows.Forms.GroupBox();
-			this.logoutBtn = new System.Windows.Forms.Button();
-			this.txtServer = new System.Windows.Forms.TextBox();
-			this.loginBtn = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
-			this.applyButton = new System.Windows.Forms.Button();
-			this.groupAuth.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// groupAuth
-			// 
-			this.groupAuth.AutoSize = true;
-			this.groupAuth.Controls.Add(this.logoutBtn);
-			this.groupAuth.Controls.Add(this.txtServer);
-			this.groupAuth.Controls.Add(this.loginBtn);
-			this.groupAuth.Controls.Add(this.label1);
-			this.groupAuth.Location = new System.Drawing.Point(3, 3);
-			this.groupAuth.Name = "groupAuth";
-			this.groupAuth.Size = new System.Drawing.Size(474, 179);
-			this.groupAuth.TabIndex = 0;
-			this.groupAuth.TabStop = false;
-			this.groupAuth.Text = "Authentication";
-			// 
-			// logoutBtn
-			// 
-			this.logoutBtn.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.logoutBtn.Enabled = false;
-			this.logoutBtn.Location = new System.Drawing.Point(121, 95);
-			this.logoutBtn.Name = "logoutBtn";
-			this.logoutBtn.Size = new System.Drawing.Size(218, 23);
-			this.logoutBtn.TabIndex = 2;
-			this.logoutBtn.Text = "Logout";
-			this.logoutBtn.UseVisualStyleBackColor = true;
-			this.logoutBtn.Click += new System.EventHandler(this.logoutBtn_Click);
-			// 
-			// txtServer
-			// 
-			this.txtServer.Location = new System.Drawing.Point(121, 17);
-			this.txtServer.Name = "txtServer";
-			this.txtServer.Size = new System.Drawing.Size(218, 20);
-			this.txtServer.TabIndex = 1;
-			// 
-			// loginBtn
-			// 
-			this.loginBtn.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.loginBtn.Location = new System.Drawing.Point(121, 55);
-			this.loginBtn.Name = "loginBtn";
-			this.loginBtn.Size = new System.Drawing.Size(218, 23);
-			this.loginBtn.TabIndex = 1;
-			this.loginBtn.Text = "Login";
-			this.loginBtn.UseVisualStyleBackColor = true;
-			this.loginBtn.Click += new System.EventHandler(this.btnSave_Click);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(7, 20);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(41, 13);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Server:";
-			// 
-			// errorProvider
-			// 
-			this.errorProvider.ContainerControl = this;
-			// 
-			// applyButton
-			// 
-			this.applyButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.applyButton.Location = new System.Drawing.Point(13, 188);
-			this.applyButton.Name = "applyButton";
-			this.applyButton.Size = new System.Drawing.Size(150, 22);
-			this.applyButton.TabIndex = 3;
-			this.applyButton.Text = "Apply";
-			this.applyButton.UseVisualStyleBackColor = true;
-			this.applyButton.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// OptionsAuthCtrl
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.applyButton);
-			this.Controls.Add(this.groupAuth);
-			this.Name = "OptionsAuthCtrl";
-			this.Size = new System.Drawing.Size(598, 250);
-			this.VisibleChanged += new System.EventHandler(this.OptionsAuthCtrl_VisibleChanged);
-			this.groupAuth.ResumeLayout(false);
-			this.groupAuth.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OptionsAuthCtrl));
+            this.groupAuth = new System.Windows.Forms.GroupBox();
+            this.applyButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.logoutBtn = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupAuth.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupAuth
+            // 
+            this.groupAuth.AutoSize = true;
+            this.groupAuth.Controls.Add(this.applyButton);
+            this.groupAuth.Controls.Add(this.pictureBox1);
+            this.groupAuth.Controls.Add(this.logoutBtn);
+            this.groupAuth.Controls.Add(this.txtServer);
+            this.groupAuth.Controls.Add(this.loginBtn);
+            this.groupAuth.Controls.Add(this.label1);
+            this.groupAuth.Location = new System.Drawing.Point(3, 3);
+            this.groupAuth.Name = "groupAuth";
+            this.groupAuth.Size = new System.Drawing.Size(391, 193);
+            this.groupAuth.TabIndex = 0;
+            this.groupAuth.TabStop = false;
+            this.groupAuth.Text = "Authentication";
+            // 
+            // applyButton
+            // 
+            this.applyButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.applyButton.Location = new System.Drawing.Point(10, 152);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(150, 22);
+            this.applyButton.TabIndex = 3;
+            this.applyButton.Text = "Apply";
+            this.applyButton.UseVisualStyleBackColor = true;
+            this.applyButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(220, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // logoutBtn
+            // 
+            this.logoutBtn.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.logoutBtn.Enabled = false;
+            this.logoutBtn.Location = new System.Drawing.Point(121, 95);
+            this.logoutBtn.Name = "logoutBtn";
+            this.logoutBtn.Size = new System.Drawing.Size(218, 23);
+            this.logoutBtn.TabIndex = 2;
+            this.logoutBtn.Text = "Logout";
+            this.logoutBtn.UseVisualStyleBackColor = true;
+            this.logoutBtn.Click += new System.EventHandler(this.logoutBtn_Click);
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(121, 17);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(218, 20);
+            this.txtServer.TabIndex = 1;
+            this.txtServer.Text = "https://checkmarx.voegol.com.br/";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.loginBtn.Location = new System.Drawing.Point(121, 55);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(218, 23);
+            this.loginBtn.TabIndex = 1;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server:";
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // OptionsAuthCtrl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupAuth);
+            this.Name = "OptionsAuthCtrl";
+            this.Size = new System.Drawing.Size(598, 250);
+            this.VisibleChanged += new System.EventHandler(this.OptionsAuthCtrl_VisibleChanged);
+            this.groupAuth.ResumeLayout(false);
+            this.groupAuth.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -136,5 +151,6 @@ namespace CxViewerAction.Views.DockedView
         private System.Windows.Forms.ErrorProvider errorProvider;
 		private System.Windows.Forms.Button logoutBtn;
 		private System.Windows.Forms.Button applyButton;
-	}
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
 }
